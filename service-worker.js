9d7eb661474aa44aa40f2c5d74ac9ea0c5e75120
sat050s0ra-cloud <sat0.50.s0ra@gmail.com>
2026-09-15 22:12:53 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='casec-sprint-v15';
+const CACHE='casec-sprint-v16';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
